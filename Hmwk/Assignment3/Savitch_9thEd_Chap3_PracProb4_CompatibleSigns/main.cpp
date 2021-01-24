b7d41d3774b39123318451a2990bd0525f5f6dce
scottrenfrow01 <77753517+scottrenfrow01@users.noreply.github.com>
2021-01-23 23:25:18 -0800

    Add files via upload

---
+++ Hmwk/Assignment3/Savitch_9thEd_Chap3_PracProb4_CompatibleSigns/main.cpp
@@ -0,0 +1,330 @@
+/* 
+ * File:   main.cpp
+ * Author: Scott Renfrow
+ * Created on January 12, 2021, 7:50 PM
+ * Purpose:  Compatible Signs Problem
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+#include <string>
+using namespace std;
+
+//User Libraries
+
+//Global Constants, no Global Variables are allowed
+//Math/Physics/Conversions/Higher Dimensions - i.e. PI, e, etc...
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Set the random number seed
+    
+    //Declare Variables
+    string inp1, //The first inputted Earth sign 
+           inp2; //The second inputted Earth sign
+    
+    //Initialize or input i.e. set variable values
+    cout<<"Horoscope Program which examines compatible signs."<<endl;
+    cout<<"Input 2 signs."<<endl;
+    cin>>inp1;
+    cin>>inp2;
+
+    //Map inputs -> outputs
+    
+    //Display the outputs
+    
+    if (inp1=="Taurus" && inp2=="Aries") {
+        cout<<"Taurus and Aries are not compatible signs.";
+    }if (inp1=="Taurus" && inp2=="Leo") {
+        cout<<"Taurus and Leo are not compatible signs.";
+    }if (inp1=="Taurus" && inp2=="Sagittarius") {
+        cout<<"Taurus and Sagittarius are not compatible signs.";
+    }if (inp1=="Taurus" && inp2=="Virgo") {
+        cout<<"Taurus and Virgo are compatible Earth signs.";
+    }if (inp1=="Taurus" && inp2=="Capricorn") {
+        cout<<"Taurus and Capricorn are compatible Earth signs.";
+    }if (inp1=="Taurus" && inp2=="Gemini") {
+        cout<<"Taurus and Gemini are not compatible signs.";
+    }if (inp1=="Taurus" && inp2=="Libra") {
+        cout<<"Taurus and Libra are not compatible signs.";
+    }if (inp1=="Taurus" && inp2=="Aquarius") {
+        cout<<"Taurus and Aquarius are not compatible signs.";
+    }if (inp1=="Taurus" && inp2=="Cancer") {
+        cout<<"Taurus and Cancer are not compatible signs.";
+    }if (inp1=="Taurus" && inp2=="Scorpio") {
+        cout<<"Taurus and Scorpio are not compatible signs.";
+    }if (inp1=="Taurus" && inp2=="Pisces") {
+        cout<<"Taurus and Pisces are not compatible signs.";
+    }if (inp1=="Taurus" && inp2=="Taurus") {
+        cout<<"Taurus and Taurus are compatible Earth signs.";
+    }if (inp1=="Aries" && inp2=="Taurus") {
+        cout<<"Aries and Taurus are not compatible signs.";
+    }if (inp1=="Aries" && inp2=="Leo") {
+        cout<<"Aries and Leo are compatible Earth signs.";
+    }if (inp1=="Aries" && inp2=="Sagittarius") {
+        cout<<"Aries and Sagittarius are compatible Fire signs.";
+    }if (inp1=="Aries" && inp2=="Virgo") {
+        cout<<"Aries and Virgo are not compatible signs.";
+    }if (inp1=="Aries" && inp2=="Capricorn") {
+        cout<<"Aries and Capricorn are not compatible signs.";
+    }if (inp1=="Aries" && inp2=="Gemini") {
+        cout<<"Aries and Gemini are not compatible signs.";
+    }if (inp1=="Aries" && inp2=="Libra") {
+        cout<<"Aries and Libra are not compatible signs.";
+    }if (inp1=="Aries" && inp2=="Aquarius") {
+        cout<<"Aries and Aquarius are not compatible signs.";
+    }if (inp1=="Aries" && inp2=="Cancer") {
+        cout<<"Aries and Cancer are not compatible signs.";
+    }if (inp1=="Aries" && inp2=="Scorpio") {
+        cout<<"Aries and Scorpio are not compatible signs.";
+    }if (inp1=="Aries" && inp2=="Pisces") {
+        cout<<"Aries and Pisces are not compatible signs.";
+    }if (inp1=="Aries" && inp2=="Aries") {
+        cout<<"Aries and Aries are compatible Fire signs.";
+    }if (inp1=="Leo" && inp2=="Taurus") {
+        cout<<"Leo and Taurus are not compatible signs.";
+    }if (inp1=="Leo" && inp2=="Aries") {
+        cout<<"Leo and Aries are compatible Fire signs.";
+    }if (inp1=="Leo" && inp2=="Sagittarius") {
+        cout<<"Leo and Sagittarius are compatible Fire signs.";
+    }if (inp1=="Leo" && inp2=="Virgo") {
+        cout<<"Leo and Virgo are not compatible signs.";
+    }if (inp1=="Leo" && inp2=="Capricorn") {
+        cout<<"Leo and Capricorn are not compatible signs.";
+    }if (inp1=="Leo" && inp2=="Gemini") {
+        cout<<"Leo and Gemini are not compatible signs.";
+    }if (inp1=="Leo" && inp2=="Libra") {
+        cout<<"Leo and Libra are not compatible signs.";
+    }if (inp1=="Leo" && inp2=="Aquarius") {
+        cout<<"Leo and Aquarius are not compatible signs.";
+    }if (inp1=="Leo" && inp2=="Cancer") {
+        cout<<"Leo and Cancer are not compatible signs.";
+    }if (inp1=="Leo" && inp2=="Scorpio") {
+        cout<<"Leo and Scorpio are not compatible signs.";
+    }if (inp1=="Leo" && inp2=="Pisces") {
+        cout<<"Leo and Pisces are not compatible signs.";
+    }if (inp1=="Leo" && inp2=="Leo") {
+        cout<<"Leo and Leo are compatible Fire signs.";
+    }if (inp1=="Sagittarius" && inp2=="Taurus") {
+        cout<<"Sagittarius and Taurus are not compatible signs.";
+    }if (inp1=="Sagittarius" && inp2=="Aries") {
+        cout<<"Sagittarius and Aries are compatible Fire signs.";
+    }if (inp1=="Sagittarius" && inp2=="Leo") {
+        cout<<"Sagittarius and Leo are compatible Fire signs.";
+    }if (inp1=="Sagittarius" && inp2=="Virgo") {
+        cout<<"Sagittarius and Virgo are not compatible signs.";
+    }if (inp1=="Sagittarius" && inp2=="Capricorn") {
+        cout<<"Sagittarius and Capricorn are not compatible signs.";
+    }if (inp1=="Sagittarius" && inp2=="Gemini") {
+        cout<<"Sagittarius and Gemini are not compatible signs.";
+    }if (inp1=="Sagittarius" && inp2=="Libra") {
+        cout<<"Sagittarius and Libra are not compatible signs.";
+    }if (inp1=="Sagittarius" && inp2=="Aquarius") {
+        cout<<"Sagittarius and Aquarius are not compatible signs.";
+    }if (inp1=="Sagittarius" && inp2=="Cancer") {
+        cout<<"Sagittarius and Cancer are not compatible signs.";
+    }if (inp1=="Sagittarius" && inp2=="Scorpio") {
+        cout<<"Sagittarius and Scorpio are not compatible signs.";
+    }if (inp1=="Sagittarius" && inp2=="Pisces") {
+        cout<<"Sagittarius and Pisces are not compatible signs.";
+    }if (inp1=="Sagittarius" && inp2=="Sagittarius") {
+        cout<<"Sagittarius and Sagittarius are compatible Fire signs.";
+    }if (inp1=="Virgo" && inp2=="Aries") {
+        cout<<"Virgo and Aries are not compatible signs.";
+    }if (inp1=="Virgo" && inp2=="Leo") {
+        cout<<"Virgo and Leo are not compatible signs.";
+    }if (inp1=="Virgo" && inp2=="Sagittarius") {
+        cout<<"Virgo and Sagittarius are not compatible signs.";
+    }if (inp1=="Virgo" && inp2=="Taurus") {
+        cout<<"Virgo and Tarus are compatible Earth signs.";
+    }if (inp1=="Virgo" && inp2=="Capricorn") {
+        cout<<"Virgo and Capricorn are compatible Earth signs.";
+    }if (inp1=="Virgo" && inp2=="Gemini") {
+        cout<<"Virgo and Gemini are not compatible signs.";
+    }if (inp1=="Virgo" && inp2=="Libra") {
+        cout<<"Virgo and Libra are not compatible signs.";
+    }if (inp1=="Virgo" && inp2=="Aquarius") {
+        cout<<"Virgo and Aquarius are not compatible signs.";
+    }if (inp1=="Virgo" && inp2=="Cancer") {
+        cout<<"Virgo and Cancer are not compatible signs.";
+    }if (inp1=="Virgo" && inp2=="Scorpio") {
+        cout<<"Virgo and Scorpio are not compatible signs.";
+    }if (inp1=="Virgo" && inp2=="Pisces") {
+        cout<<"Virgo and Pisces are not compatible signs.";
+    }if (inp1=="Virgo" && inp2=="Virgo") {
+        cout<<"Virgo and Virgo are compatible Earth signs.";
+    }if (inp1=="Capricorn" && inp2=="Aries") {
+        cout<<"Capricorn and Aries are not compatible signs.";
+    }if (inp1=="Capricorn" && inp2=="Leo") {
+        cout<<"Capricorn and Leo are not compatible signs.";
+    }if (inp1=="Capricorn" && inp2=="Sagittarius") {
+        cout<<"Capricorn and Sagittarius are not compatible signs.";
+    }if (inp1=="Capricorn" && inp2=="Taurus") {
+        cout<<"Capricorn and Taurus are compatible Earth signs.";
+    }if (inp1=="Capricorn" && inp2=="Virgo") {
+        cout<<"Capricorn and Virgo are compatible Earth signs.";
+    }if (inp1=="Capricorn" && inp2=="Gemini") {
+        cout<<"Capricorn and Gemini are not compatible signs.";
+    }if (inp1=="Capricorn" && inp2=="Libra") {
+        cout<<"Capricorn and Libra are not compatible signs.";
+    }if (inp1=="Capricorn" && inp2=="Aquarius") {
+        cout<<"Capricorn and Aquarius are not compatible signs.";
+    }if (inp1=="Capricorn" && inp2=="Cancer") {
+        cout<<"Capricorn and Cancer are not compatible signs.";
+    }if (inp1=="Capricorn" && inp2=="Scorpio") {
+        cout<<"Capricorn and Scorpio are not compatible signs.";
+    }if (inp1=="Capricorn" && inp2=="Pisces") {
+        cout<<"Capricorn and Pisces are not compatible signs.";
+    }if (inp1=="Capricorn" && inp2=="Capricorn") {
+        cout<<"Capricorn and Capricorn are compatible Earth signs.";
+    }if (inp1=="Gemini" && inp2=="Aries") {
+        cout<<"Gemini and Aries are not compatible signs.";
+    }if (inp1=="Gemini" && inp2=="Leo") {
+        cout<<"Gemini and Leo are not compatible signs.";
+    }if (inp1=="Gemini" && inp2=="Sagittarius") {
+        cout<<"Gemini and Sagittarius are not compatible signs.";
+    }if (inp1=="Gemini" && inp2=="Taurus") {
+        cout<<"Gemini and Taurus are not compatible signs.";
+    }if (inp1=="Gemini" && inp2=="Virgo") {
+        cout<<"Gemini and Virgo are not compatible signs.";
+    }if (inp1=="Gemini" && inp2=="Capricorn") {
+        cout<<"Gemini and Capricorn are not compatible signs.";
+    }if (inp1=="Gemini" && inp2=="Libra") {
+        cout<<"Gemini and Libra are compatible Air signs.";
+    }if (inp1=="Gemini" && inp2=="Aquarius") {
+        cout<<"Gemini and Aquarius are compatible Air signs.";
+    }if (inp1=="Gemini" && inp2=="Cancer") {
+        cout<<"Gemini and Cancer are not compatible signs.";
+    }if (inp1=="Gemini" && inp2=="Scorpio") {
+        cout<<"Gemini and Scorpio are not compatible signs.";
+    }if (inp1=="Gemini" && inp2=="Pisces") {
+        cout<<"Gemini and Pisces are not compatible signs.";
+    }if (inp1=="Gemini" && inp2=="Gemini") {
+        cout<<"Gemini and Gemini are compatible Air signs.";
+    }if (inp1=="Libra" && inp2=="Aries") {
+        cout<<"Libra and Aries are not compatible signs.";
+    }if (inp1=="Libra" && inp2=="Leo") {
+        cout<<"Libra and Leo are not compatible signs.";
+    }if (inp1=="Libra" && inp2=="Sagittarius") {
+        cout<<"Libra and Sagittarius are not compatible signs.";
+    }if (inp1=="Libra" && inp2=="Taurus") {
+        cout<<"Libra and Taurus are not compatible signs.";
+    }if (inp1=="Libra" && inp2=="Virgo") {
+        cout<<"Libra and Virgo are not compatible signs.";
+    }if (inp1=="Libra" && inp2=="Capricorn") {
+        cout<<"Libra and Capricorn are not compatible signs.";
+    }if (inp1=="Libra" && inp2=="Gemini") {
+        cout<<"Libra and Gemini are compatible Air signs.";
+    }if (inp1=="Libra" && inp2=="Aquarius") {
+        cout<<"Libra and Aquarius are compatible Air signs.";
+    }if (inp1=="Libra" && inp2=="Cancer") {
+        cout<<"Libra and Cancer are not compatible signs.";
+    }if (inp1=="Libra" && inp2=="Scorpio") {
+        cout<<"Libra and Scorpio are not compatible signs.";
+    }if (inp1=="Libra" && inp2=="Pisces") {
+        cout<<"Libra and Pisces are not compatible signs.";
+    }if (inp1=="Libra" && inp2=="Libra") {
+        cout<<"Libra and Libra are compatible Air signs.";
+    }if (inp1=="Aquarius" && inp2=="Aries") {
+        cout<<"Aquarius and Aries are not compatible signs.";
+    }if (inp1=="Aquarius" && inp2=="Leo") {
+        cout<<"Aquarius and Leo are not compatible signs.";
+    }if (inp1=="Aquarius" && inp2=="Sagittarius") {
+        cout<<"Aquarius and Sagittarius are not compatible signs.";
+    }if (inp1=="Aquarius" && inp2=="Taurus") {
+        cout<<"Aquarius and Taurus are not compatible signs.";
+    }if (inp1=="Aquarius" && inp2=="Virgo") {
+        cout<<"Aquarius and Virgo are not compatible signs.";
+    }if (inp1=="Aquarius" && inp2=="Capricorn") {
+        cout<<"Aquarius and Capricorn are not compatible signs.";
+    }if (inp1=="Aquarius" && inp2=="Gemini") {
+        cout<<"Aquarius and Libra are compatible Air signs.";
+    }if (inp1=="Aquarius" && inp2=="Libra") {
+        cout<<"Aquarius and Libra are compatible Air signs.";
+    }if (inp1=="Aquarius" && inp2=="Cancer") {
+        cout<<"Aquarius and Cancer are not compatible signs.";
+    }if (inp1=="Aquarius" && inp2=="Scorpio") {
+        cout<<"Aquarius and Scorpio are not compatible signs.";
+    }if (inp1=="Aquarius" && inp2=="Pisces") {
+        cout<<"Aquarius and Pisces are not compatible signs.";
+    }if (inp1=="Aquarius" && inp2=="Aquarius") {
+        cout<<"Aquarius and Aquarius are compatible Air signs.";
+    }if (inp1=="Cancer" && inp2=="Aries") {
+        cout<<"Cancer and Aries are not compatible signs.";
+    }if (inp1=="Cancer" && inp2=="Leo") {
+        cout<<"Cancer and Leo are not compatible signs.";
+    }if (inp1=="Cancer" && inp2=="Sagittarius") {
+        cout<<"Cancer and Sagittarius are not compatible signs.";
+    }if (inp1=="Cancer" && inp2=="Taurus") {
+        cout<<"Cancer and Taurus are not compatible signs.";
+    }if (inp1=="Cancer" && inp2=="Virgo") {
+        cout<<"Cancer and Virgo are not compatible signs.";
+    }if (inp1=="Cancer" && inp2=="Capricorn") {
+        cout<<"Cancer and Capricorn are not compatible signs.";
+    }if (inp1=="Cancer" && inp2=="Gemini") {
+        cout<<"Cancer and Libra are not compatible signs.";
+    }if (inp1=="Cancer" && inp2=="Libra") {
+        cout<<"Cancer and Libra are not compatible  signs.";
+    }if (inp1=="Cancer" && inp2=="Aquarius") {
+        cout<<"Cancer and Aquarius are not compatible signs.";
+    }if (inp1=="Cancer" && inp2=="Scorpio") {
+        cout<<"Cancer and Scorpio are compatible Water signs.";
+    }if (inp1=="Cancer" && inp2=="Pisces") {
+        cout<<"Cancer and Pisces are compatible Water signs.";
+    }if (inp1=="Cancer" && inp2=="Cancer") {
+        cout<<"Cancer and Cancer are compatible Water signs.";
+    }if (inp1=="Scorpio" && inp2=="Aries") {
+        cout<<"Scorpio and Aries are not compatible signs.";
+    }if (inp1=="Scorpio" && inp2=="Leo") {
+        cout<<"Scorpio and Leo are not compatible signs.";
+    }if (inp1=="Scorpio" && inp2=="Sagittarius") {
+        cout<<"Scorpio and Sagittarius are not compatible signs.";
+    }if (inp1=="Scorpio" && inp2=="Taurus") {
+        cout<<"Scorpio and Taurus are not compatible signs.";
+    }if (inp1=="Scorpio" && inp2=="Virgo") {
+        cout<<"Scorpio and Virgo are not compatible signs.";
+    }if (inp1=="Scorpio" && inp2=="Capricorn") {
+        cout<<"Scorpio and Capricorn are not compatible signs.";
+    }if (inp1=="Scorpio" && inp2=="Gemini") {
+        cout<<"Scorpio and Libra are not compatible signs.";
+    }if (inp1=="Scorpio" && inp2=="Libra") {
+        cout<<"Scorpio and Libra are not compatible  signs.";
+    }if (inp1=="Scorpio" && inp2=="Aquarius") {
+        cout<<"Scorpio and Aquarius are not compatible signs.";
+    }if (inp1=="Scorpio" && inp2=="Cancer") {
+        cout<<"Scorpio and Cancer are compatible Water signs.";
+    }if (inp1=="Scorpio" && inp2=="Pisces") {
+        cout<<"Scorpio and Pisces are compatible Water signs.";
+    }if (inp1=="Scorpio" && inp2=="Scorpio") {
+        cout<<"Scorpio and Scorpio are compatible Water signs.";
+    }if (inp1=="Pisces" && inp2=="Aries") {
+        cout<<"Pisces and Aries are not compatible signs.";
+    }if (inp1=="Pisces" && inp2=="Leo") {
+        cout<<"Pisces and Leo are not compatible signs.";
+    }if (inp1=="Pisces" && inp2=="Sagittarius") {
+        cout<<"Pisces and Sagittarius are not compatible signs.";
+    }if (inp1=="Pisces" && inp2=="Taurus") {
+        cout<<"Pisces and Taurus are not compatible signs.";
+    }if (inp1=="Pisces" && inp2=="Virgo") {
+        cout<<"Pisces and Virgo are not compatible signs.";
+    }if (inp1=="Pisces" && inp2=="Capricorn") {
+        cout<<"Pisces and Capricorn are not compatible signs.";
+    }if (inp1=="Pisces" && inp2=="Gemini") {
+        cout<<"Pisces and Libra are not compatible signs.";
+    }if (inp1=="Pisces" && inp2=="Libra") {
+        cout<<"Pisces and Libra are not compatible signs.";
+    }if (inp1=="Pisces" && inp2=="Aquarius") {
+        cout<<"Pisces and Aquarius are not compatible signs.";
+    }if (inp1=="Pisces" && inp2=="Cancer") {
+        cout<<"Pisces and Cancer are compatible Water signs.";
+    }if (inp1=="Pisces" && inp2=="Scorpio") {
+        cout<<"Pisces and Scorpio are compatible Water signs.";
+    }if (inp1=="Pisces" && inp2=="Pisces") {
+        cout<<"Pisces and Pisces are compatible Water signs.";
+    }
+
+    //Exit stage right or left!
+    return 0;
+}
