a5dc824d30b4772e4ab6380b2ae6b36c27bd2927
scottrenfrow01 <77753517+scottrenfrow01@users.noreply.github.com>
2021-01-23 23:11:00 -0800

    Add files via upload

---
+++ Hmwk/Assignment1/Gaddis_9thEd_Chap2_Prob15_TrianglePattern/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Author: Scott Renfrow
+ * Created on January 8, 2021, 5:04 PM
+ * Purpose:  Triangle Pattern Problem
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    cout<<"   *"<<endl;
+    cout<<"  ***"<<endl;
+    cout<<" *****"<<endl;
+    cout<<"*******"<<endl;
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
